75859f801074d78335981b3c27cb118572001be4
JGreen <2501545@uad.ac.uk>
2025-11-14 21:32:10 +0000

    Created Camera script

---
+++ CMP112_U2_GreenJ/CMP112_U2_GreenJ/Assets/cameraScript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class cameraScript : MonoBehaviour
+{
+
+    public Transform Player;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            transform.position = new Vector3(Player.position.x + 5, Player.position.y + 2, -10);
+
+    }
+}
