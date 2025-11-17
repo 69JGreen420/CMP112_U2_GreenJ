a3d9b359fb9f3e0d7cff5aceeeda3bcca08cc9b7
69JGreen420 <JoshGeeeeee@outlook.com>
2025-11-17 19:55:50 +0000

    Added prefabs + created playerDie script

---
+++ CMP112_U2_GreenJ/CMP112_U2_GreenJ/Assets/playerDie.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class playerDie : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("deathBarrier") || collision.gameObject.CompareTag("Spike"))
+        {
+
+            die();
+
+        }
+
+    }
+
+    void die() {
+
+        Destroy(gameObject);
+        Debug.Log("Player died!");
+
+    }
+
+}
