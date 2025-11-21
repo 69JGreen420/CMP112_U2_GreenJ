a26a3a5cbf08ac6d74e78adf9c208f2294d8cea7
69JGreen420 <JoshGeeeeee@outlook.com>
2025-11-21 14:13:36 +0000

    Added (unworking) UI

---
+++ CMP112_U2_GreenJ/CMP112_U2_GreenJ/Assets/Scripts/playerDie.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class playerDie : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("deathBarrier") || collision.gameObject.CompareTag("Spike"))
+        {
+
+            die();
+
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("deathBarrier"))
+        {
+
+            die();
+
+        }
+
+    }
+
+    void die() {
+
+        Destroy(gameObject);
+        Debug.Log("Player died!");
+
+    }
+
+}
